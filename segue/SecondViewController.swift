664b4bfeb6f464c99f0ef3eaeb2c845206ec2057
M-e-e-n-a <113291512+M-e-e-n-a@users.noreply.github.com>
2022-10-13 12:26:30 +0530

    Add files via upload

---
+++ segue/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  segue
+//
+//  Created by ISEAPLAB20 on 13/10/22.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
---
+++ segue/segue/ThirdViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ThirdViewController.swift
+//  segue
+//
+//  Created by ISEAPLAB20 on 13/10/22.
+//
+
+import Foundation
